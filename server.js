7a769b9a07645838009dbb16f6d4daba17aa5147
ahnniething <ahnniething@gmail.com>
2022-08-03 21:29:06 +0900

    feat: add apollo server

---
+++ server.js
@@ -0,0 +1,21 @@
+const {ApolloServer, gql} = require("apollo-server");
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }`;
+
+const resolvers = {
+    Query: {
+        hello: () => "world",
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(() => console.log("Server is running on http://localhost:4000"))
+
+
