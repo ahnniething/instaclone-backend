69d9325074dca5bb93257b9e3eebdf79aa6967d4
ahnniething <ahnniething@gmail.com>
2022-09-01 14:08:38 +0900

    #5 feat: add the user model to schema

--- movies/movies.queries.js
+++
@@ -1,6 +0,0 @@
-export default {
-    Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where:{ id }}),
-    }
-};
---
+++ users/users.typeDefs.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type User {
+    id: String!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    createdAt: String!
+    updateAt: String!
+  }
+
+  type Mutation {
+    createAccount(firstName: String!, lastName: String, username: String!, email: String!, password: String!): User
+  }
+
+  type Query {
+    seeProfile(username: String!): User
+  }
+`;
