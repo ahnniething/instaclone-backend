2219a7b87f18439c4dd3875bdf153f5569fc60d2
ahnniething <ahnniething@gmail.com>
2022-09-01 16:53:43 +0900

    #6 feat: check password with args.password

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -43,14 +43,22 @@ export default {
     },
     login: async (_, { username, password }) => {
       // find user with args.username
-      const user = await client.user.findFirst({ where: username });
+      const user = await client.user.findFirst({ where: { username } });
       if (!user) {
         return {
           ok: false,
-          error: "User not found"
+          error: "User not found."
         };
       }
       // check password with args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password."
+        };
+      }
+
       // issue a token and send it to the user
     }
   }
