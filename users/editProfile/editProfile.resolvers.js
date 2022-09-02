335617287dafe8f45f34c8e4a273fae148b9fd7c
ahnniething <ahnniething@gmail.com>
2022-09-02 16:58:24 +0900

    #6 feat: add mutation that edits profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,38 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi")
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUSer = await client.user.update({
+        where: {
+          id: 1
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword })
+        }
+      });
+      if (updatedUSer) {
+        return {
+          ok: true
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile."
+        };
+      }
+    }
   }
 };
