0a65ce04a9d6b39c20c16c0352c5e53aebbf5fac
ahnniething <ahnniething@gmail.com>
2022-09-02 12:13:51 +0900

    #6 refactor: change the architecture

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
-import client from "../client";
+import client from "../../client";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -41,30 +40,6 @@ export default {
       } catch (e) {
         return e;
       }
-    },
-    login: async (_, { username, password }) => {
-      // find user with args.username
-      const user = await client.user.findFirst({ where: { username } });
-      if (!user) {
-        return {
-          ok: false,
-          error: "User not found."
-        };
-      }
-      // check password with args.password
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect password."
-        };
-      }
-      // issue a token(jwt) and send it to the user
-      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token
-      };
     }
   }
 };
