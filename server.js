ddb49a8d3dc19ac69350eafe91936358bd4be43f
ahnniething <ahnniething@gmail.com>
2022-09-04 15:26:54 +0900

    #6 feat: update argument context to function to get authorization token from http request

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ import schema from "./schema";
 
 const server = new ApolloServer({
   schema,
-  context: {
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyMzQ2MTQwfQ.NKDzgGRQYqkWn5Jxg0o8P2w_q-bOq93Bn4pM3w_xwAo"
+  context: ({ req }) => {
+    console.log(req.headers);
+    return {
+      token: req.headers.authorization
+    };
   }
 });
 
