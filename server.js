416a7d03241850bda03f0a0b9cfef53a845854e4
ahnniething <ahnniething@gmail.com>
2022-09-04 15:13:32 +0900

    #6 feat: send token using the argument context object

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 
 import { ApolloServer } from "apollo-server";
-import schema from './schema'
-
+import schema from "./schema";
 
 const server = new ApolloServer({
-    schema,
+  schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyMzQ2MTQwfQ.NKDzgGRQYqkWn5Jxg0o8P2w_q-bOq93Bn4pM3w_xwAo"
+  }
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 server
-.listen(PORT)
-.then(() => console.log(`Server is running on http://localhost:${PORT}`))
-
-
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}`));
