b701f4cde9e7ff862c55a199a7ed061f119395cb
ahnniething <ahnniething@gmail.com>
2022-09-05 17:01:53 +0900

    #6 refactor: update a fucntion edits profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,42 +2,42 @@ import client from "../../client";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
 
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword },
+  { loggedInUser, protectResolver }
+) => {
+  protectResolver(loggedInUser);
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUSer = await client.user.update({
+    where: {
+      id: loggedInUser.id
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(uglyPassword && { password: uglyPassword })
+    }
+  });
+  if (updatedUSer) {
+    return {
+      ok: true
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile."
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password: newPassword },
-        { loggedInUser, protectResolver }
-      ) => {
-        protectResolver(loggedInUser);
-        let uglyPassword = null;
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUSer = await client.user.update({
-          where: {
-            id: loggedInUser.id
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(uglyPassword && { password: uglyPassword })
-          }
-        });
-        if (updatedUSer) {
-          return {
-            ok: true
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Could not update profile."
-          };
-        }
-      }
-    )
+    editProfile: protectedResolver(resolverFn)
   }
 };
