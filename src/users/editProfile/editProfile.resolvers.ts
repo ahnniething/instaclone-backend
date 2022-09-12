8d9e8a2f92f95ae7973946abe3efa85a909131eb
ahnniething <ahnniething@gmail.com>
2022-09-12 10:40:32 +0900

    add avatar parameter and get file stream using createReadStream() from uploaded file

--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -2,12 +2,18 @@ import * as bcrypt from "bcrypt";
 import { Resolver, Resolvers } from "../../types";
 import { protectedResolver } from "../users.utils";
 import { GraphQLUpload } from "graphql-upload";
+import { createReadStream } from "fs";
+import { AvatarFile } from "../../shared/shared.interfaces";
 
 const resolverFn: Resolver = async (
   _,
-  { firstName, lastName, username, email, password: newPassword, bio },
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser, client }
 ) => {
+  // console.log("::editProfile resolver:::avatar", avatar);
+  const { filename, createReadStream }: AvatarFile = await avatar.file;
+  const stream = createReadStream();
+  console.log("::editProfile resolver:::stream", stream);
   let uglyPassword = null;
   if (newPassword) {
     uglyPassword = await bcrypt.hash(newPassword, 10);
