53acfefeebd02785114c6f574eceb30ee30c80bd
ahnniething <ahnniething@gmail.com>
2022-09-18 21:13:45 +0900

    feat: update follow resolvers and handle errors

--- src/users/followUser/followUser.resolvers.ts
+++ src/users/followUser/followUser.resolvers.ts
@@ -5,6 +5,13 @@ import { protectedResolver } from "../users.utils";
 export default {
   Mutation: {
     followUser: protectedResolver(async (_, { username }, { loggedInUser }) => {
+      const ok = await client.user.findUnique({ where: { username } });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "That user does not exist.",
+        };
+      }
       await client.user.update({
         where: {
           id: loggedInUser?.id,
