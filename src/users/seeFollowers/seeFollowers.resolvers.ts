058fa3d3de1c7e56fa07b66d150f354af5d4ea95
ahnniething <ahnniething@gmail.com>
2022-09-24 20:32:17 +0900

    feat: add totalpages to get followers by offset pagination

--- src/users/seeFollowers/seeFollowers.resolvers.ts
+++ src/users/seeFollowers/seeFollowers.resolvers.ts
@@ -1,23 +1,32 @@
 import client from "../../client";
 import { Resolvers, Context } from "../../types";
 
-interface SeeFollowersArgs {
-  username: string;
-  cursor?: string;
-}
-
 const resolvers: Resolvers = {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({
           take: 5,
           skip: (page - 1) * 5,
         });
+      const totalFollwers = await client.user.count({
+        where: { following: { some: { username } } },
+      });
       return {
         ok: true,
         followers,
+        totalPages: Math.ceil(totalFollwers / 5),
       };
     },
   },
